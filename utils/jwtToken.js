0132f2dbd1397fdd4998c884fbf730a3f3efa6b3
NiladriModak <niladrimodakk2001@gmail.com>
2024-01-14 01:17:27 +0530

    yesh

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,7 +4,7 @@ const sendToken=(user,statusCode,res)=>{
     const options={
         expires:new Date(Date.now()+(5*24*60*60*1000)),
         httpOnly:true,
-        secure:false,
+        secure:true,
         sameSite: none
     }
     res.status(statusCode).cookie("token",token,options).json({
