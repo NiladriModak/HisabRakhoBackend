bed2ee170d1398cf417e01b9e51408253ba44dc8
NiladriModak <niladrimodakk2001@gmail.com>
2024-01-14 00:15:04 +0530

    cookies

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.use(cors(
     credentials: true
   }
 ))
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://hisab-rakho-frontend-ux6f.vercel.app'); // Replace with your frontend URL
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 dotenv.config();
 
 app.use(cookieParser()); //this is user to handle the cookie send by the client and make available for req.cookies
