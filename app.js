9a5a67037d44fcfe6d6d7ab0ab61fcdb13f8c0bf
NiladriModak <niladrimodakk2001@gmail.com>
2024-01-14 01:11:55 +0530

    tryinggg

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ app.use(cors(
   }
 ))
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://hisab-rakho-frontend-ux6f.vercel.app'); // Replace with your frontend URL
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://hisab-rakho-frontend-ux6f.vercel.app'); // Replace with your frontend URL
+//   res.header('Access-Control-Allow-Credentials', true);
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 dotenv.config();
 
--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,8 +4,8 @@ const sendToken=(user,statusCode,res)=>{
     const options={
         expires:new Date(Date.now()+(5*24*60*60*1000)),
         httpOnly:true,
-        secure:true,
-        sameSite: 'Lax'
+        secure:false,
+        sameSite: none
     }
     res.status(statusCode).cookie("token",token,options).json({
         success:true,
